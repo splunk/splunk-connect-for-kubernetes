fd1f0d25f22a6a523f114fcc0d7cc4bd7dba7ee4
Shubham Jain <shujain@splunk.com>
2019-01-17 15:27:18 -0800

    add cicd step to deploy k8s-connect

---
+++ .circleci/deploy_connector.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+echo "Setup kube client..."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo apt-get -y install gnupg
+sudo mkdir ~/.kube
+echo $GPG_KEY | gpg --output config --passphrase-fd 0 --decrypt .circleci/kubeconfig.gpg
+sudo mv config ~/.kube/config
+helm install --name=ci-$CIRCLE_SHA1 -f .circleci/sck_values.yml helm-artifacts/splunk-connect-for-kubernetes*.tgz
+#Todo-need to add cleanup process after adding tests.
