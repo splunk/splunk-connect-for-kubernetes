04dc17637154d150ad482741f78dc1121a252a94
Shubham Jain <shujain@splunk.com>
2019-01-17 16:52:41 -0800

    added ci deployment pipeline

--- .circleci/push_sck_artifacts.sh
+++ .circleci/push_sck_artifacts.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
-sudo apt-get install -y python-pip libpython-dev > /dev/null 2>&1
-echo "Installing aws cli..."
-sudo pip install awscli > /dev/null 2>&1
 echo "Pushing SCK artifacts to s3..."
 aws s3 cp helm-artifacts/splunk-kubernetes-logging*.tgz s3://k8s-ci-artifacts/
 aws s3 cp helm-artifacts/splunk-kubernetes-metrics*.tgz s3://k8s-ci-artifacts/
