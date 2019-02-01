e913f13770c686fede8710636a7c1a9d9b6ce1e6
Glen Patzlaff <glen@splunk.com>
2019-02-01 14:32:43 -0800

    Updates to address review feedback

--- test/k8s_metrics_tests/test_config.py
+++ test/k8s_metrics_tests/test_config.py
@@ -8,16 +8,16 @@ from ..common import check_events_from_splunk
 from ..common import check_metrics_from_splunk
 
 
-@pytest.mark.parametrize("test_input,expected", [
-    ("test_kube_node_cpu_usage_total", 1)
+@pytest.mark.parametrize("metric,dimension", [
+    ("kube.pod.cpu.usage.seconds.total", "pod_name"),
+    ("kube.node.cpu.usage.total", "node"),
+    ("kube.container.cpu.system.seconds.total", "namespace")
 ])
-def test_metric_name(setup, test_input, expected):
+def test_metric_name(setup, metric, dimension):
     '''
-    Clustername is a configurable parameter, test that user specified cluster-name is attached as a metadata to all the logs
+    This test covers one metric from each endpoint that the metrics plugin covers
     '''
-    logging.info("testing test_kube_node_cpu_usage_total input={0} \
-                expected={1} event(s)".format(test_input, expected))
-    logging.getLogger().info("Hey")
+    logging.info("testing for presence of metric={0}".format(metric))
 
     events = check_metrics_from_splunk(start_time="-24h@h",
                                   end_time="now",
@@ -25,8 +25,8 @@ def test_metric_name(setup, test_input, expected):
                                   user=setup["splunk_user"],
                                   password=setup["splunk_password"],
                                   index="circleci_metrics",
-                                  dimension="node",
-                                  metric_name="kube.node.cpu.usage.total")
+                                  dimension=dimension,
+                                  metric_name=metric)
     logging.info("Splunk received %s events in the last minute",
                          len(events))
     assert len(events) > 0
