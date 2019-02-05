5e6a2dca71e94820082df1c8fcb94fdbbdcbbecc
Glen Patzlaff <glen@splunk.com>
2019-02-05 15:42:58 -0800

    Added more test coverage

--- test/k8s_metrics_tests/test_config.py
+++ test/k8s_metrics_tests/test_config.py
@@ -9,9 +9,62 @@ from ..common import check_metrics_from_splunk
 
 
 @pytest.mark.parametrize("metric,dimension", [
-    ("kube.pod.cpu.usage.seconds.total", "pod_name"),
-    ("kube.node.cpu.usage.total", "node"),
-    ("kube.container.cpu.system.seconds.total", "namespace")
+    #Aggregator Metrics
+    ("kube.container.cpu.limit", "host"),
+    ("kube.container.memory.request", "host"),
+    ("kube.pod.cpu.limit", "host"),
+    ("kube.pod.memory.request", "host"),
+    ("kube.namespace.cpu.limit", "host"),
+    ("kube.namespace.memory.request", "host"),
+    ("kube.cluster.cpu.limit", "host"),
+    ("kube.cluster.memory.request", "host"),
+    ("kube.node.cpu.capacity", "host"),
+    ("kube.node.memory.utilization", "host"),
+    #Summary Metrics
+    ("kube.container.uptime", "host"),
+    ("kube.container.cpu.usage", "host"),
+    ("kube.container.memory.usage_bytes", "host"),
+    ("kube.container.rootfs.available_bytes", "host"),
+    ("kube.container.logs.used_bytes", "host"),
+    ("kube.container.cpu.usage", "host"),
+    ("kube.container.cpu.usage", "host"),
+    ("kube.container.cpu.usage", "host"),
+    ("kube.container.cpu.usage", "host"),
+    ("kube.container.memory.usage.bytes", "host"),
+    ("kube.node.cpu.usage", "host"),
+    ("kube.node.memory.usage", "host"),
+    ("kube.node.uptime", "host"),
+    ("kube.node.network.rx_bytes", "host"),
+    ("kube.node.fs.available_bytes", "host"),
+    ("kube.node.imagefs.available_bytes", "host"),
+    ("kube.node.runtime.imagefs.maxpid", "host"),
+    ("kube.pod.uptime", "host"),
+    ("kube.pod.cpu.usage", "host"),
+    ("kube.pod.memory.usage_bytes", "host"),
+    ("kube.pod.network.rx_bytes", "host"),
+    ("kube.pod.ephemeral-storage.available_bytes", "host"),
+    ("kube.pod.volume.available_bytes", "host"),
+    ("kube.sys-container.cpu.usage", "node"),
+    ("kube.sys-container.memory.usage_bytes", "host"),
+    ("kube.sys-container.uptime", "host"),
+    ("kube.container.cpu.usage.seconds.total", "host"),
+    #Stats Metrics
+    ("kube.node.cpu.cfs.periods", "host"),
+    ("kube.node.diskio.io_service_bytes.stats.Read", "host"),
+    ("kube.node.filesystem.available", "host"),
+    ("kube.node.memory.cache", "host"),
+    ("kube.node.tasks_stats.nr_running", "host"),
+    #Cadvisor Metrics
+    ("kube.container.cpu.load.average.10s", "host"),
+    ("kube.container.fs.inodes.free", "host"),
+    ("kube.container.fs.reads.total", "host"),
+    ("kube.container.fs.usage.bytes", "host"),
+    ("kube.container.fs.writes.total", "host"),
+    ("kube.container.last.seen", "host"),
+    ("kube.container.memory.usage.bytes", "host"),
+    ("kube.container.network.receive.bytes.total", "host"),
+    ("kube.container.spec.cpu.period", "host"),
+    ("kube.container.tasks.state", "host")
 ])
 def test_metric_name(setup, metric, dimension):
     '''
