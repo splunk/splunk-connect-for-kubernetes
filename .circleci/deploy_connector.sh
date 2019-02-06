5b009b0e0e4299a5e6f5f62f38ba39e2dde976d5
Shubham Jain <shujain@splunk.com>
2019-02-06 15:01:36 -0800

    added a logic to check and clean previous deployment

--- .circleci/deploy_connector.sh
+++ .circleci/deploy_connector.sh
@@ -8,7 +8,15 @@ sudo apt-get -y install gnupg
 sudo mkdir ~/.kube
 echo $GPG_KEY | gpg --output config --passphrase-fd 0 --decrypt .circleci/kubeconfig.gpg
 sudo mv config ~/.kube/config
+#Make sure to check and clean previously failed deployment
+echo "Checking if previous deployment exist..."
+if [ "`helm ls`" == "" ]; then
+   echo "Nothing to clean, ready for deployment"
+else
+   helm delete --purge $(helm ls --short)
+fi
+echo "Deploying k8s-connect with latest changes"
 helm install --name=ci-$CIRCLE_SHA1 -f .circleci/sck_values.yml helm-artifacts/splunk-connect-for-kubernetes*.tgz
 #Todo-need to add test cases and run them before cleaning the deployment
-#wait for deployment to finish
-sleep 30
+#wait for deployment to finish, sleep longer for metrics-aggr to start sending metrics
+sleep 90
