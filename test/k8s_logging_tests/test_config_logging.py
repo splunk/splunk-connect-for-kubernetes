11ef20fa4dc6018629fd0c67f4682a346b9dc461
Glen Patzlaff <glen@splunk.com>
2019-02-11 17:58:28 -0800

    Updated logging func test to include sourcetype and source tests

--- test/k8s_logging_tests/test_config_logging.py
+++ test/k8s_logging_tests/test_config_logging.py
@@ -138,3 +138,52 @@ def test_namespace_routing(setup, test_input, expected):
     logging.getLogger().info("Splunk received {0} events in the last minute in the index {1}".format(len(events), index))
 
     assert len(events) > 0
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("kube:kube-apiserver", 1),
+    ("fluentd:monitor-agent", 1),
+    ("kube:container:splunk-fluentd-k8s-metrics-agg", 1),
+    ("kube:container:splunk-fluentd-k8s-metrics", 1),
+    ("kube:container:splunk-fluentd-k8s-logs", 1),
+    ("kube:dns-controller", 1),
+    ("kube:etcd", 1),
+    ("kube:container:splunk-fluentd-k8s-objects", 1),
+    ("kube:container:tiller", 1),
+    ("kube:kube-controller-manager", 1)
+])
+def test_sourcetype(setup, test_input, expected):
+    '''
+    Test that known sourcetypes are present in target index
+    '''
+    logging.getLogger().info("testing for presence of sourcetype={0} \
+                expected={1} event(s)".format(test_input, expected))
+    events = check_events_from_splunk(start_time="-24h@h",
+                                  url=setup["splunkd_url"],
+                                  user=setup["splunk_user"],
+                                  query=["search index=circleci_events sourcetype={0}".format(test_input)],
+                                  password=setup["splunk_password"])
+    logging.getLogger().info("Splunk received %s events in the last minute",
+                         len(events))
+    assert len(events) >= expected
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("/var/log/containers/kube-apiserver-ip*", 1),
+    ("/var/log/containers/ci*", 1),
+    ("namespace:default/pod:ci*", 1),
+    ("/var/log/containers/dns-controller*", 1),
+    ("/var/log/containers/etcd-server-ip*", 1)
+])
+def test_source(setup, test_input, expected):
+    '''
+    Test that known sources are present in target index
+    '''
+    logging.getLogger().info("testing for presence of sourcetype={0} \
+                expected={1} event(s)".format(test_input, expected))
+    events = check_events_from_splunk(start_time="-24h@h",
+                                  url=setup["splunkd_url"],
+                                  user=setup["splunk_user"],
+                                  query=["search index=circleci_events source={0}".format(test_input)],
+                                  password=setup["splunk_password"])
+    logging.getLogger().info("Splunk received %s events in the last minute",
+                         len(events))
+    assert len(events) >= expected
