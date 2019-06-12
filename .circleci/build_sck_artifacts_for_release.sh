41d9573e25d15e7167f43083273102e4dca84521
Chaitanya Phalak <cphalak@splunk.com>
2019-06-11 17:48:13 -0700

    fix

--- .circleci/build_sck_artifacts_for_release.sh
+++ .circleci/build_sck_artifacts_for_release.sh
@@ -3,28 +3,28 @@ set -e
 TAG=`cat VERSION`
 
 # Install yq yaml parser
-wget https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64
-sudo chmod +x yq_linux_amd64
-sudo mv yq_linux_amd64 /usr/local/bin/yq
-
-yq w -i helm-chart/splunk-kubernetes-logging/values.yaml image.name splunk/fluentd-hec:1.1.1
-yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml image.name splunk/k8s-metrics:1.1.1
-yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml imageAgg.name splunk/k8s-metrics-aggr:1.1.0
-yq w -i helm-chart/splunk-kubernetes-objects/values.yaml image.name splunk/kube-objects:1.1.0
-
-yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-kubernetes-metrics/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-kubernetes-objects/Chart.yaml version $VERSION
-
-yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-kubernetes-metrics/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-kubernetes-objects/Chart.yaml appVersion $VERSION
-
-yq w -i helm-chart/splunk-kubernetes-logging/requirements.yaml dependencies.splunk-kubernetes-logging.version $VERSION
-yq w -i helm-chart/splunk-kubernetes-metrics/requirements.yaml dependencies.splunk-kubernetes-metrics.version $VERSION
-yq w -i helm-chart/splunk-kubernetes-objects/requirements.yaml dependencies.splunk-kubernetes-objects.version $VERSION
+#wget https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64
+#sudo chmod +x yq_linux_amd64
+#sudo mv yq_linux_amd64 /usr/local/bin/yq
+#
+#yq w -i helm-chart/splunk-kubernetes-logging/values.yaml image.name splunk/fluentd-hec:1.1.1
+#yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml image.name splunk/k8s-metrics:1.1.1
+#yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml imageAgg.name splunk/k8s-metrics-aggr:1.1.0
+#yq w -i helm-chart/splunk-kubernetes-objects/values.yaml image.name splunk/kube-objects:1.1.0
+#
+#yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml version $VERSION
+#yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml version $VERSION
+#yq w -i helm-chart/splunk-kubernetes-metrics/Chart.yaml version $VERSION
+#yq w -i helm-chart/splunk-kubernetes-objects/Chart.yaml version $VERSION
+#
+#yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml appVersion $VERSION
+#yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml appVersion $VERSION
+#yq w -i helm-chart/splunk-kubernetes-metrics/Chart.yaml appVersion $VERSION
+#yq w -i helm-chart/splunk-kubernetes-objects/Chart.yaml appVersion $VERSION
+#
+#yq w -i helm-chart/splunk-kubernetes-logging/requirements.yaml dependencies.splunk-kubernetes-logging.version $VERSION
+#yq w -i helm-chart/splunk-kubernetes-metrics/requirements.yaml dependencies.splunk-kubernetes-metrics.version $VERSION
+#yq w -i helm-chart/splunk-kubernetes-objects/requirements.yaml dependencies.splunk-kubernetes-objects.version $VERSION
 
 mkdir helm-artifacts-release
 if [[ -d "helm-chart/splunk-connect-for-kubernetes/charts" ]]; then
