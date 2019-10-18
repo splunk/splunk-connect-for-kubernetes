0286ccf624701b2a6f308f3edd1bd8e6155ff57a
Rock Baek <rbaek@splunk.com>
2019-10-18 15:09:39 -0700

    test

--- .circleci/run_functional_tests.sh
+++ .circleci/run_functional_tests.sh
@@ -5,7 +5,7 @@ sudo pip install --upgrade pip
 sudo pip install -r requirements.txt
 #Run pytests
 echo "Running functional tests....."
-python3 -m pytest \
+python -m pytest \
 	--splunkd-url https://$CI_SPLUNK_HEC_HOST:8089 \
 	--splunk-user admin \
 	--splunk-password $CI_SPLUNK_PASSWORD \
