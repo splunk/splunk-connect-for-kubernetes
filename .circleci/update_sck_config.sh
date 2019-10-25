475f1aa2d456fb78f8581bf25a184fcc11253aef
Rock Baek <rbaek@splunk.com>
2019-10-24 17:10:35 -0700

    uncomment temporarily commented code lines

--- .circleci/update_sck_config.sh
+++ .circleci/update_sck_config.sh
@@ -4,16 +4,16 @@ set -e
 source PLUGIN_VERSIONS.sh
 
 # Install yq yaml parser
-# wget https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64
-# sudo chmod +x yq_linux_amd64
-# sudo mv yq_linux_amd64 /usr/local/bin/yq
+wget https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64
+sudo chmod +x yq_linux_amd64
+sudo mv yq_linux_amd64 /usr/local/bin/yq
 
 # Modify splunk environment values
-# yq w -i .circleci/sck_values.yml global.splunk.hec.host $CI_SPLUNK_HEC_HOST
-# yq w -i .circleci/sck_values.yml global.splunk.hec.token $CI_SPLUNK_HEC_TOKEN
-# yq w -i .circleci/performance/perf_test_sck_values.yml global.splunk.hec.host $CI_SPLUNK_HEC_HOST_PERF
-# yq w -i .circleci/performance/perf_test_sck_values.yml global.splunk.hec.token $CI_SPLUNK_HEC_TOKEN_PERF
-# yq w -i .circleci/sck_values.yml global.splunk.hec.protocol ${CI_HEC_PROTOCOL:-https}
+yq w -i .circleci/sck_values.yml global.splunk.hec.host $CI_SPLUNK_HEC_HOST
+yq w -i .circleci/sck_values.yml global.splunk.hec.token $CI_SPLUNK_HEC_TOKEN
+yq w -i .circleci/performance/perf_test_sck_values.yml global.splunk.hec.host $CI_SPLUNK_HEC_HOST_PERF
+yq w -i .circleci/performance/perf_test_sck_values.yml global.splunk.hec.token $CI_SPLUNK_HEC_TOKEN_PERF
+yq w -i .circleci/sck_values.yml global.splunk.hec.protocol ${CI_HEC_PROTOCOL:-https}
 
 # Modify docker images to be used
 yq w -i .circleci/sck_values.yml splunk-kubernetes-logging.image.name splunk/fluentd-hec
