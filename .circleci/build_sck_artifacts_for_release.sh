4bd31e74a24cf52ed7d64ef315fc3ef1681ba2f4
Rock Baek <rbaek@splunk.com>
2019-11-04 16:07:01 -0800

    updating manifests process

--- .circleci/build_sck_artifacts_for_release.sh
+++ .circleci/build_sck_artifacts_for_release.sh
@@ -1,6 +1,6 @@
-yq#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
-TAG=`cat VERSION`
+VERSION=`cat VERSION`
 
 source PLUGIN_VERSIONS.sh
 
@@ -9,10 +9,12 @@ wget https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64
 sudo chmod +x yq_linux_amd64
 sudo mv yq_linux_amd64 /usr/local/bin/yq
 
-yq w -i helm-chart/splunk-kubernetes-logging/values.yaml image.name splunk/fluentd-hec:$FLUENTD_HEC_VERSION
-yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml image.name splunk/k8s-metrics:$K8S_METRICS_VERISION
-yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml imageAgg.name splunk/k8s-metrics-aggr:$K8S_METRICS_AGGR_VERSION
-yq w -i helm-chart/splunk-kubernetes-objects/values.yaml image.name splunk/kube-objects:$KUBE_OBJECT_VERSION
+# Before release, "make manifests" call should have been run. 
+# then there are nothing else to change. but keeping them here just in case 
+yq w -i helm-chart/splunk-kubernetes-logging/values.yaml image.tag $FLUENTD_HEC_VERSION
+yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml image.tag $K8S_METRICS_VERISION
+yq w -i helm-chart/splunk-kubernetes-metrics/values.yaml imageAgg.tag $K8S_METRICS_AGGR_VERSION
+yq w -i helm-chart/splunk-kubernetes-objects/values.yaml image.tag $KUBE_OBJECT_VERSION
 
 yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml version $VERSION
 yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml version $VERSION
@@ -24,9 +26,9 @@ yq w -i helm-chart/splunk-kubernetes-logging/Chart.yaml appVersion $VERSION
 yq w -i helm-chart/splunk-kubernetes-metrics/Chart.yaml appVersion $VERSION
 yq w -i helm-chart/splunk-kubernetes-objects/Chart.yaml appVersion $VERSION
 
-yq w -i helm-chart/splunk-kubernetes-logging/requirements.yaml dependencies.splunk-kubernetes-logging.version $VERSION
-yq w -i helm-chart/splunk-kubernetes-metrics/requirements.yaml dependencies.splunk-kubernetes-metrics.version $VERSION
-yq w -i helm-chart/splunk-kubernetes-objects/requirements.yaml dependencies.splunk-kubernetes-objects.version $VERSION
+yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[0].version $VERSION
+yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[1].version $VERSION
+yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[2].version $VERSION
 
 mkdir helm-artifacts-release
 if [[ -d "helm-chart/splunk-connect-for-kubernetes/charts" ]]; then
--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+include PLUGIN_VERSIONS.sh
+export $(shell sed 's/=.*//' PLUGIN_VERSIONS.sh)
+
 connect-chart: logging-chart objects-chart metrics-chart
 	@sed -i.bak -E -e 's/^([[:space:]]+repository:[[:space:]]+).+$$/\1local/' helm-chart/splunk-connect-for-kubernetes/requirements.yaml
 	@mv helm-chart/splunk-connect-for-kubernetes/requirements.yaml.bak .
@@ -21,6 +24,7 @@ build:
 
 .PHONY: manifests
 manifests: connect-chart
+	@./tools/update_charts_version.sh
 	@helm template \
 	   --set global.splunk.hec.host=MY-SPLUNK-HOST \
 	   --set global.splunk.hec.token=MY-SPLUNK-TOKEN \
@@ -29,6 +33,10 @@ manifests: connect-chart
 	   --set splunk-kubernetes-metrics.fullnameOverride="splunk-kubernetes-metrics" \
 	   --set splunk-kubernetes-objects.fullnameOverride="splunk-kubernetes-objects" \
 	   --set splunk-kubernetes-objects.kubernetes.insecureSSL=true \
+	   --set splunk-kubernetes-objects.image.tag=$(KUBE_OBJECT_VERSION) \
+	   --set splunk-kubernetes-logging.image.tag=$(FLUENTD_HEC_VERSION) \
+	   --set splunk-kubernetes-metrics.image.tag=$(K8S_METRICS_VERISION) \
+	   --set splunk-kubernetes-metrics.imageAgg.tag=$(K8S_METRICS_AGGR_VERSION) \
 	   $$(ls build/splunk-connect-for-kubernetes-*.tgz) \
 	   | ruby tools/gen_manifest.rb manifests
 
