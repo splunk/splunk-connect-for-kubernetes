d11b84e345bfd1c2aa62d920491e3772d50d3e6a
Rock Baek <rbaek@splunk.com>
2019-12-13 12:31:40 -0800

    re-structure helm chart sub folders

--- .circleci/build_sck_artifacts.sh
+++ .circleci/build_sck_artifacts.sh
@@ -1,40 +1,27 @@
 #!/usr/bin/env bash
 set -e
-VERSION=`cat VERSION`
+VERSION=$(cat VERSION)
 TAG="$VERSION-${CIRCLE_SHA1}"
 
-function replace_generic_version ()
-{
+function replace_generic_version() {
   file="$1"
-  _line=`awk '/version:/{print NR;exit}' $file`
+  _line=$(awk '/version:/{print NR;exit}' $file)
   replacement="version: $TAG"
   # Escape backslash, forward slash and ampersand for use as a sed replacement.
-  replacement_escaped=$( echo "$replacement" | sed -e 's/[\/&]/\\&/g' )
+  replacement_escaped=$(echo "$replacement" | sed -e 's/[\/&]/\\&/g')
   sed -i "${_line}s/.*/$replacement_escaped/" "$file"
 }
 
-repos_array=( "helm-chart/splunk-connect-for-kubernetes" "helm-chart/splunk-kubernetes-logging"
-              "helm-chart/splunk-kubernetes-metrics" "helm-chart/splunk-kubernetes-objects" )
-sub_repos_array=( "helm-chart/splunk-kubernetes-logging"
-              "helm-chart/splunk-kubernetes-metrics" "helm-chart/splunk-kubernetes-objects" )
+repos_array=("helm-chart/splunk-connect-for-kubernetes" "helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-logging"
+  "helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-metrics" "helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-objects")
 
-for repo in "${repos_array[@]}"
-do
+for repo in "${repos_array[@]}"; do
   filename="${repo}/Chart.yaml"
   replace_generic_version $filename
 done
 
 mkdir helm-artifacts
-mkdir helm-chart/splunk-connect-for-kubernetes/charts
 
-for sub_repo in "${sub_repos_array[@]}"
-do
-  cp -rp $sub_repo helm-chart/splunk-connect-for-kubernetes/charts
-done
-
-for repo in "${repos_array[@]}"
-do
+for repo in "${repos_array[@]}"; do
   helm package -d helm-artifacts $repo
 done
-
-rm -rf helm-chart/splunk-connect-for-kubernetes/charts
--- .circleci/push_sck_artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Pushing SCK artifacts to s3..."
-aws s3 cp helm-artifacts/splunk-kubernetes-logging*.tgz s3://k8s-ci-artifacts/
-aws s3 cp helm-artifacts/splunk-kubernetes-metrics*.tgz s3://k8s-ci-artifacts/
-aws s3 cp helm-artifacts/splunk-kubernetes-objects*.tgz s3://k8s-ci-artifacts/
-aws s3 cp helm-artifacts/splunk-connect-for-kubernetes*.tgz s3://k8s-ci-artifacts/
