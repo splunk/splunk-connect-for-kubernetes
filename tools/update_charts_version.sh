5bdefe8911d037797360b1afc4bd3c3917f78474
rockb1017 <rockb1017@gmail.com>
2021-11-04 18:18:38 -0700

    1.4.10

--- tools/update_charts_version.sh
+++ tools/update_charts_version.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=`cat VERSION`
+export VERSION=`cat VERSION`
 
 if ! command -v yq &> /dev/null
 then
     echo "yq could not be found. Please install yq first"
+    echo "for MacOS, brew install yq"
     exit 1
 fi
 
 echo "VERSION $VERSION"
 echo "Updating Charts' version"
 
-yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-logging/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-metrics/Chart.yaml version $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-objects/Chart.yaml version $VERSION
+yq e -i '.version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/Chart.yaml 
+yq e -i '.version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-logging/Chart.yaml
+yq e -i '.version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-metrics/Chart.yaml
+yq e -i '.version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-objects/Chart.yaml
 
-yq w -i helm-chart/splunk-connect-for-kubernetes/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-logging/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-metrics/Chart.yaml appVersion $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-objects/Chart.yaml appVersion $VERSION
+yq e -i '.appVersion = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/Chart.yaml
+yq e -i '.appVersion = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-logging/Chart.yaml
+yq e -i '.appVersion = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-metrics/Chart.yaml
+yq e -i '.appVersion = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/charts/splunk-kubernetes-objects/Chart.yaml
 
-yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[0].version $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[1].version $VERSION
-yq w -i helm-chart/splunk-connect-for-kubernetes/requirements.yaml dependencies[2].version $VERSION
+yq e -i '.dependencies[0].version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/requirements.yaml
+yq e -i '.dependencies[1].version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/requirements.yaml
+yq e -i '.dependencies[2].version = env(VERSION)' helm-chart/splunk-connect-for-kubernetes/requirements.yaml
 
